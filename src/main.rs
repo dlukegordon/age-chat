b292c4610845aab0788ecfef6664ab18cfdeb43a
Lucas Gordon <luke@valhalla>
2025-02-04 18:37:55 -0600

    Add cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+    cli.run()?;
+    Ok(())
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Subcommands,
+}
+
+#[derive(Subcommand)]
+enum Subcommands {
+    /// Run the chat server
+    Serve,
+}
+
+impl Cli {
+    fn run(self) -> Result<()> {
+        match self.command {
+            Subcommands::Serve => serve()?,
+        }
+        Ok(())
+    }
+}
+
+fn serve() -> Result<()> {
+    println!("Hello world!");
+    Ok(())
 }
